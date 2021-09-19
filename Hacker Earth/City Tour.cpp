09d2a8345fe8630988f9986c7f58a784f8f07a47
sohagraha <sohagrahabd@gmail.com>
2021-09-19 18:55:55 +0600

    19-9-2021

---
+++ Hacker Earth/City Tour.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   long long int n;
+   cin >> n;
+   while(n--)
+   {
+    long long int t;
+    cin >> t;
+    cout << t*4 << endl;
+   }
+}
+
+// City Tour
+// https://www.hackerearth.com/problem/algorithm/city-tour/
+
+        
